83d6ca072732cc59c661acb99b51ea130bb1458b
James Pickard <james.pickard@gmail.com>
2015-10-05 17:44:22 -0400

    CSS updates and fixes

--- wwwroot/js/gdb.js
+++ wwwroot/js/gdb.js
@@ -1,4 +1,4 @@
-/*! greendragonbungay.co.uk - v0.1.0 - 2015-10-03
+/*! greendragonbungay.co.uk - v0.1.0 - 2015-10-05
 * https://github.com/euoia/greendragonbungay.co.uk/
 * Copyright (c) 2015 James Pickard; Licensed MIT */
 /*! jQuery v2.1.0 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
