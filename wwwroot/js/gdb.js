fd4ea5291a2555b8a396b999e6cd2615307c4580
James Pickard <james.pickard@gmail.com>
2023-03-22 12:36:16 +0000

    Rebuild JS

--- wwwroot/js/gdb.js
+++ wwwroot/js/gdb.js
@@ -1,4 +1,4 @@
-/*! greendragonbungay.co.uk - v0.1.0 - 2023-03-17
+/*! greendragonbungay.co.uk - v0.1.0 - 2023-03-22
 * https://github.com/euoia/greendragonbungay.co.uk/
 * Copyright (c) 2023 James Pickard; Licensed MIT */
 /*! jQuery v2.1.0 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
