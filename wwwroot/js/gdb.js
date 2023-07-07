ff97c6a4e9878131d111a93a6f3808860b308d51
James Pickard <james.pickard@gmail.com>
2023-07-07 03:22:32 +0100

    Wrap the quote in clientOnly

--- wwwroot/js/gdb.js
+++ wwwroot/js/gdb.js
@@ -1,4 +1,4 @@
-/*! greendragonbungay.co.uk - v0.1.0 - 2023-03-22
+/*! greendragonbungay.co.uk - v0.1.0 - 2023-05-06
 * https://github.com/euoia/greendragonbungay.co.uk/
 * Copyright (c) 2023 James Pickard; Licensed MIT */
 /*! jQuery v2.1.0 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license */
